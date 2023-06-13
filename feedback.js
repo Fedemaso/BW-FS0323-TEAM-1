eede00b5f523d92ca267e183058ba9e83dc13f81
Claudio Galiano <claudiogal92@gmail.com>
2023-06-13 17:25:00 +0200

    Primo commit

---
+++ feedback.js
@@ -0,0 +1,32 @@
+
+const ratingStars = [...document.getElementsByClassName("starSvg")];
+
+function executeRating(stars) {
+    const starClassActive = "starSvg"
+    const starClassInactive = "starSvg"
+    const starsLength = stars.length
+    let i;
+    stars.map((star) => {
+        star.onclick = () => {
+           i = stars.indexOf(star)
+            if (star.className===starClassInactive) {        
+              for (i; i >= 0; --i) stars[i].className = starClassActive
+           } else {
+              for (i; i < starsLength; ++i) stars[i].className = starClassInactive
+           }
+        }
+     })
+}
+
+
+executeRating(ratingStars)
+
+console.log(ratingStars);
+// let feedbackIn = document.getElementById('feedback-input')
+// feedbackIn.addEventListener('keydown', function(e){
+//   if(e.key === 'Enter'){
+//     // Aggiungere js per il submit del form
+//   }
+// })
+
+
