9c9716c57837fd6cb19124104b0ab58de1d915bf
Claudio Galiano <claudiogal92@gmail.com>
2023-06-14 18:16:07 +0200

    Pulizia codice feedback.js

--- js/feedback.js
+++ js/feedback.js
@@ -5,24 +5,8 @@ const starClassActive = "starActive"
 const starClassInactive = "starInactive"
 const starsLength = ratingStars.length
 
-function executeRating(stars) {
-    const starClassActive = "starSvg"
-    const starClassInactive = "starSvg"
-    const starsLength = stars.length
-    let i;
-    stars.map((star) => {
-        star.onclick = () => {
-           i = stars.indexOf(star)
-            if (star.className===starClassInactive) {        
-              for (i; i >= 0; --i) stars[i].className = starClassActive
-           } else {
-              for (i; i < starsLength; ++i) stars[i].className = starClassInactive
-           }
-        }
-     })
-}
-
-const starActive = (stars) => {
+const returRating = (stars) => {
+   let i
    stars.map((star) => {
       star.onclick = () => {
          result = stars.indexOf(star)
@@ -37,7 +21,5 @@ const starActive = (stars) => {
    })
 }
 
-executeRating(ratingStars)
-
-starActive(ratingStars)
+returRating(ratingStars)
 
