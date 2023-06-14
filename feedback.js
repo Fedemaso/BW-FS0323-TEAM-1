31966ae8c2076673dd80a1e0bc676d2d1ee2051a
Claudio Galiano <claudiogal92@gmail.com>
2023-06-14 16:36:47 +0200

    Star rating aggiunto

--- feedback.js
+++ feedback.js
@@ -1,35 +1,23 @@
 
-const ratingStars = Array.from(document.getElementsByClassName('starSvg'))
+const ratingStars = Array.from(document.getElementsByClassName('starInactive'))
 
-console.log(ratingStars);
+const starClassActive = "starActive"
+const starClassInactive = "starInactive"
+const starsLength = ratingStars.length
 
-
-function executeRating(stars) {
-    const starClassActive = "starSvg"
-    const starClassInactive = "starSvg"
-    const starsLength = stars.length
-    let i;
-    stars.map((star) => {
-        star.onclick = () => {
-           i = stars.indexOf(star)
-            if (star.className===starClassInactive) {        
-              for (i; i >= 0; --i) stars[i].className = starClassActive
-           } else {
-              for (i; i < starsLength; ++i) stars[i].className = starClassInactive
-           }
-        }
-     })
+const starActive = (stars) => {
+   stars.map((star) => {
+      star.onclick = () => {
+         result = stars.indexOf(star)
+         i = stars.indexOf(star)
+         if (star.className===starClassInactive) {
+            for (i; i >= 0; --i) stars[i].className = starClassActive
+         } else {
+            for (i; i < starsLength; ++i) stars[i].className = starClassInactive
+         }
+         console.log(result + 1);
+      }
+   })
 }
 
-
-executeRating(ratingStars)
-
-
-// let feedbackIn = document.getElementById('feedback-input')
-// feedbackIn.addEventListener('keydown', function(e){
-//   if(e.key === 'Enter'){
-//     // Aggiungere js per il submit del form
-//   }
-// })
-
-
+starActive(ratingStars)
