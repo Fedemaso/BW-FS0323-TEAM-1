0e0208329103ceafe3ebde839ed38be2d586f007
Claudio Galiano <claudiogal92@gmail.com>
2023-06-14 17:42:41 +0200

    Ricaricato feedback.js

--- feedback.js
+++ feedback.js
@@ -5,6 +5,23 @@ const starClassActive = "starActive"
 const starClassInactive = "starInactive"
 const starsLength = ratingStars.length
 
+function executeRating(stars) {
+    const starClassActive = "starSvg"
+    const starClassInactive = "starSvg"
+    const starsLength = stars.length
+    let i;
+    stars.map((star) => {
+        star.onclick = () => {
+           i = stars.indexOf(star)
+            if (star.className===starClassInactive) {        
+              for (i; i >= 0; --i) stars[i].className = starClassActive
+           } else {
+              for (i; i < starsLength; ++i) stars[i].className = starClassInactive
+           }
+        }
+     })
+}
+
 const starActive = (stars) => {
    stars.map((star) => {
       star.onclick = () => {
@@ -19,5 +36,7 @@ const starActive = (stars) => {
       }
    })
 }
+
+executeRating(ratingStars)
 
 starActive(ratingStars)
