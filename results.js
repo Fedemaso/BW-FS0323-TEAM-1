7cb723313138696d30fef7a97af43b57b0ab45c3
GabrieleColombo <133026314+GabrieleColomb@users.noreply.github.com>
2023-06-15 11:42:07 +0200

    testo centrale.js

--- results.js
+++ results.js
@@ -17,6 +17,25 @@ function updateChart() {
   config.data.datasets[0].data = [incorrectAngle, correctAngle];
   chart.update();
 }
+
+// Funzione di rendering personalizzato per il testo nel centro del grafico
+function renderText(chart) {
+  var width = chart.width;
+  var height = chart.height;
+  var ctx = chart.ctx;
+  ctx.restore();
+
+  var text = "Congratulations!"; // Testo da inserire nel centro del grafico
+  var fontSize = (height / 100).toFixed(2);
+  ctx.font = fontSize + "em sans-serif";
+  ctx.textBaseline = "middle";
+  var textX = Math.round((width - ctx.measureText(text).width) / 2);
+  var textY = height / 2;
+
+  ctx.fillText(text, textX, textY);
+  ctx.save();
+}
+
 // Configurazione iniziale del grafico
 const config = {
   type: 'doughnut',
@@ -33,14 +52,22 @@ const config = {
     cutout: 90, 
     responsive: true,
     maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        enabled: false
+      },
+      render: renderText // Funzione di rendering personalizzato
+    }
   }
-}
+};
+
 // Creazione del grafico
 const ctx = document.getElementById("chart").getContext("2d");
 const chart = new Chart(ctx, config);
 
 const validate = () => {
-  if (document.getElementById('policy').checked) {
-      window.location.href = "Benchmark.html";
-  } else alert ("Mark the checkbox to continue")
+  window.location.href = "Feedback.html";
 }
