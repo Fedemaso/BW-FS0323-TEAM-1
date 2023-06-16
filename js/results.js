e2c4f9c5389269b8e06e1779c449e8e32fb68ca4
GabrieleColombo <133026314+GabrieleColomb@users.noreply.github.com>
2023-06-16 14:27:27 +0200

    message

--- js/results.js
+++ js/results.js
@@ -55,10 +55,16 @@ const validate = () => {
 function updateChartInfo() {
   const resultText = document.getElementById("result-text");
 
-  if (quizScore < 6) {
-    resultText.innerHTML = 'Oh no! <span style="color: #D20094;">Unfortunately, you didn\'t pass the exam.</span>';
-  } else {
+  const resultBox = document.getElementById("result-text")
+  console.log(quizScore);
+  if (quizScore >= 6) {
+    resultBox.classList.add("chart-messageCorrect")
     resultText.innerHTML = `Congratulations! <span style="color: #00FFFF;">You passed the exam.</span><br>We\'ll send you the certificate in a few minutes. Check your email (including promotions/spam folder).`;
+  } else if (quizScore < 6) {
+    resultBox.classList.add("chart-messageWrong")
+    resultText.innerHTML = 'Oh no! <span style="color: #D20094;">Unfortunately, you didn\'t pass the exam.</span>';
+  } else if (quizScore >= 6) {
+    resultText.innerHTML = 'Congratulations! <span style="color: #00FFFF;">You passed the exam.</span><br>We\'ll send you the certificate in a few minutes. Check your email (including promotions/spam folder).';
   }
   const chartInfoLeft = document.getElementById("chart-info-left");
   const chartInfoRight = document.getElementById("chart-info-right");
